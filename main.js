fafe2eb8dcf1578dad22d1777aab722384628774
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-13 14:46:30 -0300

    otimizacao do codigo, armazenando informacoes do regex em um objeto

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ formulario.addEventListener('submit', function (e) {
   e.preventDefault();
 });
 
+const regexConfig = {};
+regexConfig.nome = {
+  validar: /^[A-Za-z\s]*$/,
+};
+regexConfig.telefone = {
+  validar: /^(\(?[0-9]{2}\)?)\s?([9]{1})\s?([0-9]{4})-?([0-9]{4})$/,
+  formatar: /(\d{2})([9]{1})(\d{4})(\d{4})/g,
+  formato: '($1) $2 $3-$4',
+  digitos: 11,
+};
+
 function validarInput(input) {
   let regex = '';
   if (input.id === 'nome-contato') {
-    regex = /^[A-Za-z\s]*$/;
+    regex = regexConfig.nome.validar;
   } else if (input.id === 'numero-contato') {
-    regex = /^(\(?[0-9]{2}\)?)\s?([9]{1})\s?([0-9]{4})-?([0-9]{4})$/;
+    regex = regexConfig.telefone.validar;
   } else return false;
 
   return regex.test(input.value);
